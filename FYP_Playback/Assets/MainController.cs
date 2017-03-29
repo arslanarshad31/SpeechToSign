e5b8f5ca08c4d6e676d7cd3c1a20ed813f4e4d7a
wdzhang <arslanarshad.31@gmail.com>
2017-03-29 18:48:18 +0800

    recording signs

--- FYP_Playback/Assets/MainController.cs
+++ FYP_Playback/Assets/MainController.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 using System.Threading;
 
 public class MainController : MonoBehaviour {
-	static string translatedString = "";
 	static Animator anim;
 
 	void Start () {}
@@ -19,26 +18,21 @@ public class MainController : MonoBehaviour {
 	}
 
 	private IEnumerator getTranslate(String text){
-    	WWW req = new WWW("http://localhost:8081/translate/"+ text);
+    	WWW req = new WWW("http://localhost:5000/translate/"+ text);
     	yield return req;
 		// HTTP response recieved
 		InputField translateText = GameObject.Find("/Canvas/TranslateText").GetComponent<InputField>();
 		translateText.text = req.text;
-		translatedString = req.text;
-	}
-
-	public void AnimateController() {
 		//anim = this.transform.Find("Aj").gameObject.GetComponent<Animator>();
-		string[] tokens = translatedString.ToLower().Split(' ');
-
+		string[] tokens = req.text.ToLower().Split(' ');
 		StartCoroutine(waitedAnimate(tokens));
 	}
-
+	
 	IEnumerator waitedAnimate(string[] tokens) {
 		Text displayWord = GameObject.Find("/Canvas/DisplayWord").GetComponent<Text>();
 		for (var i=0; i<tokens.Length;++i){
 			if (tokens[i].Equals(".") || tokens[i].Equals("question") || tokens[i].Equals("neutral")
-				|| tokens[i].Equals("q")) continue;
+				|| tokens[i].Equals("q") || tokens[i].Equals("exclamation")) continue;
 			Debug.Log("Setting Trig: " + tokens[i]);
 			displayWord.text = tokens[i];
 			//anim.SetTrigger(tokens[i]);
