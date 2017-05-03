48dd68165448eb8c8498084191c1d3775e2efd49
axellegupta <aguptaad@connect.ust.hk>
2017-05-04 06:05:10 +0800

    Playback updates

--- FYP_Playback/Assets/MainController.cs
+++ FYP_Playback/Assets/MainController.cs
@@ -9,35 +9,43 @@ using System.Linq;
 
 public class MainController : MonoBehaviour {
 	static Animator anim;
-	void Start () {}
+	static bool play;
+	void Start () {
+	}
 	
 	public void TranslateController(){
+		if (play) {
+			return;
+			//Toast.Create("Error Message", "Error Title").show();
+		}
 		InputField inputText = GameObject.Find("/Canvas/InputText").GetComponent<InputField>();
 		Debug.Log("Input text: " + inputText.text);
 		// Sending HTTP Request
-		StartCoroutine(getTranslate(inputText.text.Replace(' ', '@')));
+		play = true;
+		StartCoroutine(getTranslate(WWW.EscapeURL(inputText.text).Replace("+", "%20")));
 	}
 
 	private IEnumerator getTranslate(String text){
-		string request = "http://localhost:8080/translate/"+ text;
+		WWWForm form = new WWWForm();
+		 form.AddField("input",text);
+		string request = "http://arslan-test.hamza.ink/translate/" + text;
 		Debug.Log(request);
     	WWW req = new WWW(request);
     	yield return req;
 		// HTTP response recieved
-		InputField translateText = GameObject.Find("/Canvas/TranslateText").GetComponent<InputField>();
-		translateText.text = req.text;
+		//InputField translateText = GameObject.Find("/Canvas/TranslateText").GetComponent<InputField>();
+		//translateText.text = req.text;
 		anim = this.transform.Find("Aj").gameObject.GetComponent<Animator>();
 		//anim = GetComponent<Animator>();
 		string[] tokens = req.text.ToLower().Split(' ');
-		string[] nouns = {"aj", "hkust","hk","ust", "onur", "akanksha", "arslan", "sheetal", "mak", "nevin", "brian", "professor", "doctor", "zhang"};
-		string vocabulary = "NEUTRAL, QUESTION, EXCLAMATION, YOU, MORE, EAT, MOM, DAD, THANK, HI, WITH, PLAY, MUSIC, MOVIE, LIKE, FOOTBALL, YEAR, OUR, SAY, STUDY, SEE, WHERE, THINK, NEXT, NAME, ME, MY, INDIA, IT, HAVE, COME, THIS, PAST, GO, GOOD, IN, RAIN, RAINY, US, THEY, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN";
+		string[] nouns = {"aj", "hkust","hk","ust", "onur", "akanksha", "arslan", "sheetal", "mak", "nevin", "brian", "professor", "zhang"};
+		string vocabulary = "ZERO, HEAR, IDLE, NEUTRAL, QUESTION, EXCLAMATION, U, T, S, R, O, N, K, J, H, A, YOU, MORE, EAT, MOM, DAD, THANK, HI, WITH, PLAY, MUSIC, MOVIE, LIKE, FOOTBALL, YEAR, OUR, SAY, STUDY, SEE, WHERE, WHO, WHICH, THINK, NEXT, NAME, ME, MY, I, INDIA, IT, HAVE, COME, THIS, PAST, GO, GOOD, IN, RAIN, RAINY, US, THEY, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, ANGRY, BAD, BIG, BOY, CHURCH, CRY, SORRY, STORE, SUN, WATER, WORK, DRINK, GIRL, HAPPY, HOME, LOVE, MARRIAGE, SCHOOL";
 		string[] stringSeparators = new string[] {", "};
 		string[] vocab = vocabulary.ToLower().Split(stringSeparators, StringSplitOptions.RemoveEmptyEntries);
 		
 		var j = tokens.Length;
 
 		for (var i=0; i<j;++i){
-			Debug.Log(vocab.Contains(tokens[i]));
 			if (nouns.Contains(tokens[i]) || vocab.Contains(tokens[i])==false){
 				string[] letter = tokens[i].ToCharArray().Select( c => c.ToString()).ToArray();
 				List<string> myList = tokens.ToList();
@@ -48,7 +56,7 @@ public class MainController : MonoBehaviour {
 				i+= letter.Length-1;
 			}
 		}	
-		for (var i=0; i<j;++i){Debug.Log(tokens[i]);}
+		//for (var i=0; i<j;++i){Debug.Log(tokens[i]);}
 		StartCoroutine(waitedAnimate(tokens));
 	}
 	
@@ -57,22 +65,34 @@ public class MainController : MonoBehaviour {
 		Text displayWord = GameObject.Find("/Canvas/DisplayWord").GetComponent<Text>();
 		var i = 0;
 		for (i=0; i<tokens.Length;++i){
+
+			if (!play) {
+				break;
+			}
+
 			if (tokens[i].Equals("neutral") || tokens[i].Equals("question")|| tokens[i].Equals("exclamation")||tokens[i].Equals(".") ||  tokens[i].Equals("q")) {
 				if (i!=0){
 					displayWord.text = " ";
 					yield return new WaitForSeconds(1f);
 				}
+				else continue;
 			}
 			else {
 				displayWord.text = tokens[i];
 				anim.SetTrigger(Animator.StringToHash(tokens[i]));
-				yield return new WaitForSeconds(anim.GetCurrentAnimatorStateInfo(0).length);
+				if(i==1)
+					yield return new WaitForSeconds(1.5f);
+				else
+					yield return new WaitForSeconds(anim.GetCurrentAnimatorStateInfo(0).length);
 				}
 			}
-		if (i == tokens.Length){
+			play = false;
 			displayWord.text = " ";
 			anim.SetTrigger(Animator.StringToHash("idle"));
 			yield return new WaitForSeconds(anim.GetCurrentAnimatorStateInfo(0).length);
 		}
-	}
+
+	public void stopAnimation () {
+		play = false;
+		}
 }	
